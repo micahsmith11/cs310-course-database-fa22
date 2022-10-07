5eeab9a659042424bcc49f73503e84c0dc6c7e1e
Micah Smith <msmith113@stu.jsu.edu>
2022-10-07 14:01:58 -0500

    Finished project 2

--- src/edu/jsu/mcis/cs310/Database.java
+++ src/edu/jsu/mcis/cs310/Database.java
@@ -24,10 +24,46 @@ public class Database {
         
         String result = null;
         
-        // INSERT YOUR CODE HERE
         
+        // INSERT YOUR CODE HERE
+       
+     try{
+         
+
+        String query;
+        query = "SELECT * FROM section WHERE termid ="+termid+ " AND subjectid ='"+subjectid+"' AND num ='"+num+"'";
+        
+        //Prepare query
+        PreparedStatement pstmt = null;
+        pstmt = connection.prepareStatement(query);
+        //Execute query
+        boolean hasresults;
+        hasresults = pstmt.execute();
+        
+       
+        
+        //Check for Results
+        if (hasresults) {
+        ResultSet resultset;
+        //Get Results
+        resultset = pstmt.getResultSet();
+        result = getResultSetAsJSON(resultset);
+        
+       }
+
+        else {
+
+            System.err.println("ERROR: No data returned!");
+
+            }
+            
+     }
+     
+     catch (Exception e) { e.printStackTrace(); }
+       
         return result;
         
+    
     }
     
     public int register(int studentid, int termid, int crn) {
@@ -35,6 +71,31 @@ public class Database {
         int result = 0;
         
         // INSERT YOUR CODE HERE
+        String query;
+        PreparedStatement pstUpdate = null;
+        int updateCount;
+        
+        try{
+           /* Prepare Update Query */
+           
+            query = "INSERT INTO registration (studentid, termid, crn) VALUES (?, ?, ?)";
+            pstUpdate = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstUpdate.setInt(1, studentid);
+            pstUpdate.setInt(2, termid);
+            pstUpdate.setInt(3, crn);
+        
+        //Execute query
+            updateCount = pstUpdate.executeUpdate();
+        
+         /* If Update Successful, update results */
+         
+            result =  updateCount;
+         
+           
+        
+        }
+        
+        catch (Exception e) {  e.printStackTrace(); }
         
         return result;
         
@@ -45,6 +106,34 @@ public class Database {
         int result = 0;
         
         // INSERT YOUR CODE HERE
+        int updateCount;
+        PreparedStatement pstd = null;
+        
+        try{
+        /* Prepare Update Query */
+        
+            String query = "DELETE FROM registration WHERE studentid = ? AND termid = ? AND crn = ?";
+            pstd = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstd.setInt(1, studentid);
+            pstd.setInt(2, termid);
+            pstd.setInt(3, crn);
+        
+            //Execute query
+            updateCount = pstd.executeUpdate();
+         
+         
+          /* If Update Successful, update result */
+                
+                if (updateCount > 0) {
+            
+                  result = updateCount;
+
+                }
+         
+            
+        }
+        
+        catch (Exception e) { e.printStackTrace(); }
         
         return result;
         
@@ -56,6 +145,23 @@ public class Database {
         
         // INSERT YOUR CODE HERE
         
+        PreparedStatement pstUpdate = null;
+        try {
+            /* Prepare Update Query */
+            String query = "DELETE FROM registration WHERE studentid = ? AND termid = ?";
+            pstUpdate = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            pstUpdate.setInt(1, studentid);
+            pstUpdate.setInt(2, termid);
+            
+            //Excecute query
+            int updateCount = pstUpdate.executeUpdate();
+            
+            //Update results
+            result = updateCount;
+        }
+        
+        catch (Exception e) { e.printStackTrace(); }
+        
         return result;
         
     }
@@ -65,6 +171,23 @@ public class Database {
         String result = null;
         
         // INSERT YOUR CODE HERE
+        
+        try{
+            //Prepare Select query
+            String query;
+            query = "SELECT * FROM registration JOIN section ON registration.crn =section.crn;";
+            PreparedStatement pstUpdate = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS); 
+            //Execute query
+            boolean hasresults = pstUpdate.execute();
+            //Check results
+            if(hasresults) {
+                //Get results
+                ResultSet resultset = pstUpdate.getResultSet();
+                result = getResultSetAsJSON(resultset);
+            }
+        }
+        
+         catch (Exception e) { e.printStackTrace(); }
         
         return result;
         
@@ -161,6 +284,48 @@ public class Database {
             int columnCount = metadata.getColumnCount();
             
             // INSERT YOUR CODE HERE
+            
+
+                    String key, value;
+
+                  
+                    /* Get Column Names; */
+
+                    for (int i = 1; i <= columnCount; ++i) {
+
+                        key = metadata.getColumnLabel(i);
+                        keys.add(key);
+                      
+
+                    }
+
+                    /* Get Data; Print as Table Rows */
+
+                    while(resultset.next()) {
+
+                        /* Begin Next ResultSet Row */
+                        //JSONObject to connect keys and values
+                        JSONObject data = new JSONObject();
+                        
+
+                        /* Loop Through ResultSet Columns; Print Values */
+                        
+                            for (int i = 1; i <= columnCount; ++i) {
+                                
+                                /* Get Data; */
+                                value = resultset.getString(i);
+                                //Attach keys to values
+                                data.put(keys.get(i-1), value);
+                              
+                             }
+                            //Add row to Array
+                            json.add(data);
+                        
+                
+                    }
+                   
+
+               
         
         }
         catch (Exception e) { e.printStackTrace(); }
